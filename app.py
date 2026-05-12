a5403194e8fd437cc6aaeefdfe5c716cc0225961
afreed9395 <afreed9395@gmail.com>
2026-05-12 15:05:19 +0530

    added a endpoint for the graph workflow

---
+++ app.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from agent.agentic_workflow import GraphBuilder
+from fastapi.responses import JSONResponse
+import os
+app = FastAPI()
+
+
+class QueryRequest(BaseModel):
+    query: str
+
+@app.post("/query")
+async def travel_planner(query: QueryRequest):
+    
+    try:
+        print(f"Received query: {query}")
+        graph = GraphBuilder(model_provider = "openai")
+        react_app = graph()
+        png_graph = react_app.get_graph().draw_mermaid_png()
+
+        with open("my_graph.png", "wb") as f:
+            f.write(png_graph)
+
+        print(f"graph saved as my_graph.png in the current working directory {os.getcwd()}")
+        
+        messages = {"messages": [query.question]}
+        output = react_app.invoke(messages)
+
+        if isinstance(output, dict) and "messages" in output:
+            final_output = output["messages"][-1].content
+        else:
+            final_output = str(output)
+
+        return JSONResponse(content={"response": final_output})        
+    except Exception as e:
+        print(f"Error: {e}")
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+        
+
+
